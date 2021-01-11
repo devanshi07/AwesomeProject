667a013afec7f745bf44e50696709187107774dc
devanshi07 <devanshipatel.tatvasoft@gmail.com>
2021-01-11 16:14:32 +0530

    commit

--- src/res/images/index.js
+++ src/res/images/index.js
@@ -4,6 +4,12 @@ export const images = {
     IMG_SECOND : require('./../images/crop2.jpg'),
     IMG_THIRD : require('./../images/crop3.jpg'),
     IMG_FORTH : require('./../images/crop4.jpg'),
+    IMG_FIFTH : require('./../images/crop5.jpg'),
+    IMG_SIXTH : require('./../images/crop6.jpg'),
+    IMG_SEVENTH : require('./../images/crop7.jpg'),
+    IMG_EIGTH : require('./../images/crop8.jpg'),
+    IMG_NINTH : require('./../images/crop9.jpg'),
+
     IMG_BOOKMARK : require('./../images/bookmark.png'),
     IMG_HEART : require('./../images/heart.png'),
     IMG_CHAT : require('./../images/chat.png'),
--- src/screens/Assignment2/ListToGrid.js
+++ src/screens/Assignment2/ListToGrid.js
@@ -1,4 +1,4 @@
-//Article list 
+//tonggle between list and grid
 
 import React,{Component,useState,useEffect} from 'react';
 import {View,Text,Image,StyleSheet,SafeAreaView,FlatList,TouchableOpacity, Dimensions} from 'react-native';
@@ -8,7 +8,7 @@ const Item_width = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
     container: {
-        
+        flex:1,
         alignItems: "center",
         borderColor: 'black',
         borderWidth: 2
@@ -25,9 +25,23 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         paddingTop: 10,
+        color: "black"
+    },
+    headerTextstyle: {
+        fontFamily: 'Cochin',
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: "white",
+        textAlign: 'center',
+        paddingTop: 10,
         
     },
-    
+    headerStyle: {
+        width: "100%",
+        height: 50,
+        alignItems: "center",
+        backgroundColor: "red",
+    }
 });
 
 let arrayList = [
@@ -79,15 +93,16 @@ const renderItem = ({ item }) => (
                 <Text style={styles.textstyle}>{item.title}</Text>        
         </View>
   )
-function Album(){
+function ListToGrid(){
 
     const [nocolumn, setNoColumn] = useState(1);
     const [key, setKey] = useState(1);
         return(
             <SafeAreaView style={{backgroundColor: '#E8EAF6',flex:1}}>
 
-            <View style={{paddingTop: 20}}>
+            <View>
                 <TouchableOpacity
+                style={styles.headerStyle}
                 onPress={() => {
                     if(nocolumn == 1)
                     {
@@ -100,9 +115,10 @@ function Album(){
                     }
                     
                 }}>
-                    <Text style={styles.textstyle}>Change Layout</Text>
+                    <Text style={styles.headerTextstyle}>Change Layout</Text>
                 </TouchableOpacity>
                 <FlatList
+                style={{marginBottom:50}}
                 key={key}
                 numColumns={nocolumn}
                 data={arrayList}
@@ -112,4 +128,4 @@ function Album(){
         </SafeAreaView> 
         );    
 }
-export default Album;
+export default ListToGrid;
